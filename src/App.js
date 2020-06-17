f723d27a1445b9cd9beef1063894022e3eb9ea58
sbaiti <zouhaier~@gmailcom>
2020-06-17 15:55:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="App" data-test="component-app">
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('render without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent=wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1)
+})
